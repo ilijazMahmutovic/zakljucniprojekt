e02dfea7d277c86e6aa598e589767459b223bf9f
ilijazMahmutovic <mahmutovic.ilijaz@gmail.com>
2026-09-22 10:03:03 +0200

    Dropdown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Pressable, Button, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Picker } from "@react-native-picker/picker";
 const API_URL = "http://localhost:2500";
 
 const Stack = createNativeStackNavigator();
